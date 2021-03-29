27b8e8bead875513736bb0a3df4db8713520ea76
renatoacf@gmail.com <realcofre>
2021-03-29 00:05:28 -0300

    Estado e ciclo de vida

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,33 @@
-import Alert from './Alert';
+import { useState, useEffect } from 'react';
+
 
 const App = () => {
+    const [counter, setCounter] = useState(0);
+
+    useEffect(() => {
+        console.log('o contador mudou de valor');
+    }, [counter]);
+    
+
     return (
         <div className="container mt-5">
-            <Alert text="Renato"/>
-            <Alert text="Bootcamp"/>
-            
+            <button 
+                className="btn btn-primary mr-5"
+                onClick={() => setCounter(counter + 1)}
+            >
+                    +
+            </button>
+            <span>
+                {counter}
+            </span>
+            <button 
+                className="btn btn-primary ml-5"
+                onClick={() => setCounter(counter - 1)}
+            >
+                -
+            </button>
+            {counter > 5 && <h1>O valor é maior que 5</h1>}
+            {counter <= 5 && <h1>O valor é menor ou igual 5</h1>}
         </div>
     )
 }
