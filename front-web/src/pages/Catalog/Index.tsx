951f17ed1b3e130b7a29e1e3ae553bca3387422b
renatoacf@gmail.com <realcofre>
2021-04-07 02:09:37 -0300

    add axios

--- front-web/src/pages/Catalog/Index.tsx
+++ front-web/src/pages/Catalog/Index.tsx
@@ -1,30 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import ProductCard from './components/ProductCard/Index';
 import './styles.scss';
+import { makeRequest } from '../../core/utils/request';
 
-const Catalog = () => (
-    <div className="catalog-container">
-        <h1 className="catalog-title">
-            Catálogo de produtos
+const Catalog = () => {
+
+    useEffect(() => {
+        const params = {
+            page: 0,
+            linesPerPage: 12 
+        }
+        makeRequest({ url: '/products', params})
+        .then(response => console.log(response));
+    }, []);
+    return (
+        <div className="catalog-container">
+            <h1 className="catalog-title">
+                Catálogo de produtos
         </h1>
-        <div className="catalog-products">
-            <Link to="/products/1"><ProductCard /></Link>
-            <Link to="/products/2"><ProductCard /></Link>
-            <Link to="/products/3"><ProductCard /></Link>
-            <Link to="/products/4"><ProductCard /></Link>
-            <Link to="/products/5"><ProductCard /></Link>
-            <Link to="/products/6"><ProductCard /></Link>
-            <Link to="/products/7"><ProductCard /></Link>
-            <Link to="/products/8"><ProductCard /></Link>
-            
-            <Link to="/products/9"><ProductCard /></Link>
-            <Link to="/products/10"><ProductCard /></Link>
-            <Link to="/products/11"><ProductCard /></Link>
-            <Link to="/products/12"><ProductCard /></Link>
-            <Link to="/products/13"><ProductCard /></Link>
+            <div className="catalog-products">
+                <Link to="/products/1"><ProductCard /></Link>
+                <Link to="/products/2"><ProductCard /></Link>
+                <Link to="/products/3"><ProductCard /></Link>
+                <Link to="/products/4"><ProductCard /></Link>
+                <Link to="/products/5"><ProductCard /></Link>
+                <Link to="/products/6"><ProductCard /></Link>
+                <Link to="/products/7"><ProductCard /></Link>
+                <Link to="/products/8"><ProductCard /></Link>
+            </div>
         </div>
-    </div>
-);
+    );
+}
 
 export default Catalog;
