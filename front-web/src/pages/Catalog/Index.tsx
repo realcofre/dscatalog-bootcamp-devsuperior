8102f4c6efcb96ab86ed665a54e46e04102c1a4b
renatoacf@gmail.com <realcofre>
2021-04-07 02:49:36 -0300

    integrando card product

--- front-web/src/pages/Catalog/Index.tsx
+++ front-web/src/pages/Catalog/Index.tsx
@@ -1,18 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import ProductCard from './components/ProductCard/Index';
 import './styles.scss';
 import { makeRequest } from '../../core/utils/request';
+import { ProductsResponse } from '../../core/types/Product';
 
 const Catalog = () => {
 
+    const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
     useEffect(() => {
         const params = {
             page: 0,
             linesPerPage: 12 
         }
         makeRequest({ url: '/products', params})
-        .then(response => console.log(response));
+        .then(response => setProductsResponse(response.data));
     }, []);
     return (
         <div className="catalog-container">
@@ -20,14 +22,12 @@ const Catalog = () => {
                 Catálogo de produtos
         </h1>
             <div className="catalog-products">
-                <Link to="/products/1"><ProductCard /></Link>
-                <Link to="/products/2"><ProductCard /></Link>
-                <Link to="/products/3"><ProductCard /></Link>
-                <Link to="/products/4"><ProductCard /></Link>
-                <Link to="/products/5"><ProductCard /></Link>
-                <Link to="/products/6"><ProductCard /></Link>
-                <Link to="/products/7"><ProductCard /></Link>
-                <Link to="/products/8"><ProductCard /></Link>
+                {productsResponse?.content.map(product => (
+                    <Link to="/products/1" key={product.id}>
+                        <ProductCard product={product} />
+                    </Link>
+                ))}
+                
             </div>
         </div>
     );
