9c8cfec8084a0378f67528de682177f4cd573f2b
renatoacf@gmail.com <realcofre>
2021-04-08 00:58:21 -0300

    Add loaders

--- front-web/src/pages/Catalog/Index.tsx
+++ front-web/src/pages/Catalog/Index.tsx
@@ -4,17 +4,24 @@ import ProductCard from './components/ProductCard/Index';
 import './styles.scss';
 import { makeRequest } from '../../core/utils/request';
 import { ProductsResponse } from '../../core/types/Product';
+import ProductCardLoader from './components/Loaders/ProductCardLoader';
 
 const Catalog = () => {
 
     const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
+    const [isLoading, setIsLoading] = useState(false);
     useEffect(() => {
         const params = {
             page: 0,
             linesPerPage: 12 
         }
+
+        setIsLoading(true);
         makeRequest({ url: '/products', params})
-        .then(response => setProductsResponse(response.data));
+        .then(response => setProductsResponse(response.data))
+        .finally(() => {
+            setIsLoading(false);
+        })
     }, []);
     return (
         <div className="catalog-container">
@@ -22,12 +29,11 @@ const Catalog = () => {
                 Catálogo de produtos
         </h1>
             <div className="catalog-products">
-                {productsResponse?.content.map(product => (
+                { isLoading ? <ProductCardLoader /> : (productsResponse?.content.map(product => (
                     <Link to={`/products/${product.id}`} key={product.id}>
                         <ProductCard product={product} />
                     </Link>
-                ))}
-                
+                )))}
             </div>
         </div>
     );
